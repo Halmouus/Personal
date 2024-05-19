bd38a970e086ad34f34531ccb983e77592b608f3
Halmouus <javierminmaz@gmail.com>
2024-05-19 01:40:19 +0100

    Update database configuration

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://halmous:Halmous%402024@localhost/my_project_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
