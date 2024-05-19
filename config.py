ba16e522807bdccf6573c5fc5c94fa45623d7dc5
Halmouus <javierminmaz@gmail.com>
2024-05-19 23:44:26 +0100

    Secret key fixed

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = 'a_super_secret_key_for_testing_purposes'  # Fixed key for sessions
     SQLALCHEMY_DATABASE_URI = os.getenv('JAWSDB_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SECURE = True  # Ensure cookies are secure
+    SESSION_COOKIE_HTTPONLY = True  # Prevent JavaScript access to cookies
+    SESSION_COOKIE_SAMESITE = 'Lax'  # Mitigate CSRF attacks
