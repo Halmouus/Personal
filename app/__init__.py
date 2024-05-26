2fa8bb812d90e8371767b5a4559a1dba4dcc012b
Halmouus <javierminmaz@gmail.com>
2024-05-26 13:31:22 +0100

    PrfP + Status + Likes/Dislikes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.config.from_object('config.Config')
 
 db = SQLAlchemy(app)
