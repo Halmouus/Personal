f20cd4de7063e4ccf06b20d220d63275a9b88cba
Halmouus <javierminmaz@gmail.com>
2024-05-27 18:46:39 +0100

    Update run.py to use eventlet with Flask-SocketIO

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 release: flask db upgrade
 web: gunicorn run:app
 web: gunicorn run:app --preload --log-file -
+web: gunicorn -k eventlet -w 1 app:app
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from whitenoise import WhiteNoise
 from app import app
+from flask_socketio import SocketIO
+import eventlet
 
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
 app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
