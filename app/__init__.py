00801849c4e5c14a6ccc02adbd363a7117098b5e
Halmouus <javierminmaz@gmail.com>
2024-05-28 21:53:36 +0100

    debug dbl notif

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder="static")
 app.config.from_object('config.Config')
 
-socketio = SocketIO(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
